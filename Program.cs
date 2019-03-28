81b84bca09b744acdef95950b2c1f9eddc35328c
Guilherme <guilherme.santos97@outlook.com.br>
2019-03-28 15:50:32 -0300

    1.0
    Primeiro commit consumindo api - simplificado.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace consoleWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Acessando a WebAPI, aguarde um momento...");
+
+            var repositorio = new UsuarioRepositorio();
+
+            Task<List<Usuario>> usuariosTask = repositorio.GetUsuariosAsync();
+
+            usuariosTask.ContinueWith(task =>
+            {
+                var usuarios = task.Result;
+                foreach (var u in usuarios)
+                    WriteLine(u.ToString());
+
+                //Environment.Exit(0);
+            },
+            TaskContinuationOptions.OnlyOnRanToCompletion
+            );
+
+            ReadLine();
+        }
+    }
+}
---
+++ UsuarioRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace consoleWebApi
+{
+    public class UsuarioRepositorio
+    {
+        HttpClient client = new HttpClient();
+
+        public UsuarioRepositorio()
+        {
+#if DEBUG
+            client.BaseAddress = new Uri("http://localhost:33422");
+#else
+            client.BaseAddress = new Uri("");
+#endif
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<Usuario>> GetUsuariosAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("api/Usuarios");
+            if (response.IsSuccessStatusCode)
+            {
+                var dados = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<Usuario>>(dados);
+            }
+
+            return new List<Usuario>();
+        }
+    }
+}
